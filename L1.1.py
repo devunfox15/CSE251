f3b1da4628102581b89d71cc547db6957dc84513
Devun Durst <devunfoxdurst@yahoo.com>
2023-09-13 17:09:33 -0600

    new changes

---
+++ L1.1.py
@@ -0,0 +1,17 @@
+def my_function(name):
+    print("hello world")
+
+my_variable = 'akljlsh;ahap'
+
+print(my_variable)
+my_function("world")
+function_vaiable = my_function
+function_vaiable("brother kay")
+
+my_list = []
+my_list.append(1)
+my_list.append("value1", )
+print(my_list)
+
+my_dictonary = {"key": 4, "otherkey": "justin"}
+# print_dict(my_dictionary)
--- week01/team/team_threads.py
+++ week01/team/team_threads.py
@@ -1,31 +1,24 @@
-# first push
 """
 Course: CSE 251
 Lesson Week: 01 - Team Acvitiy
-File: team.py
+File: team-solution.py
 Author: Brother Comeau
 
 Purpose: Find prime numbers
-
-Instructions:
-
-- Don't include any other Python packages or modules
-- Review team activity details in I-Learn
 
 """
 
 from datetime import datetime, timedelta
 import threading
 
+
 # Include cse 251 common Python files
 from cse251 import *
 
-# Global variable for counting the number of primes found
 prime_count = 0
 numbers_processed = 0
 
-#this function determines wheter a number is a prime number 
-def is_prime(n):
+def is_prime(n: int):
     global numbers_processed
     numbers_processed += 1
 
@@ -44,25 +37,41 @@ def is_prime(n):
     return True
 
 
+def process_range(start, end):
+    global prime_count
+    for i in range(start, end):
+        if is_prime(i):
+            prime_count += 1
+            print(i, end=', ', flush=True)
+
+
 if __name__ == '__main__':
     log = Log(show_terminal=True)
     log.start_timer()
 
-    # TODO 1) Get this program running
-    # TODO 2) move the following for loop into 1 thread
-    # TODO 3) change the program to divide the for loop into 10 threads
-
     start = 10000000000
     range_count = 100000
-    for i in range(start, start + range_count):
-        if is_prime(i):
-            prime_count += 1
-            print(i, end=', ', flush=True)
-    print(flush=True)
+ #
+    number_threads = 10
+    threads = []
+    thread_range = range_count // number_threads
+
+    # Create threads and give each one a range to test
+    for i in range(10):
+        thread_start = start + (thread_range * i)
+        thread_end = thread_start + thread_range
+        t = threading.Thread(target=process_range, args=(thread_start, thread_end))
+        threads.append(t)
+
+    # Start all threads
+    for t in threads:
+        t.start()
+
+    # Wait for them to finish
+    for t in threads:
+        t.join()
 
     # Should find 4306 primes
     log.write(f'Numbers processed = {numbers_processed}')
-    log.write(f'Primes found      = {prime_count}')
+    log.write(f'Primes found = {prime_count}')
     log.stop_timer('Total time')
-
-
